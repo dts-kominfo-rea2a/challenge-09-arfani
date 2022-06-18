d26e8b09525f048551286c25d5981b7fceaf400b
arfani <arfanihidayat@gmail.com>
2022-06-18 23:42:27 +0800

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
+import Header from './components/Header'
+import Contact from './components/Contact'
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <Header />
+      <div className="content">
+        {
+          contacts.map((item, i) => {
+            return <Contact data={item} key={i} reverse={i % 2 === 0 ? true : false} />
+          })
+        }
+      </div>
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,16 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = (props) => {
     return (
-        <></>
+        <div className='contact' style={props.reverse ? {flexDirection: 'row-reverse'} : null}>
+            <img src={props.data.photo} alt={props.data.name} />
+            <div className="detail">
+                <div className="name">{props.data.name}</div>
+                <div className="phone">{props.data.phone}</div>
+                <div className="email">{props.data.email}</div>
+            </div>
+        </div>
     )
 }
 
